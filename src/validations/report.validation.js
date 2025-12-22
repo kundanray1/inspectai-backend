256b51f32c0044f0491251e526e1db1e38b297e8
kundanray1 <raykundan57@gmail.com>
2025-12-22 16:41:24 +0545

    fix: allow title and summary in report creation validation

--- src/validations/report.validation.js
+++ src/validations/report.validation.js
@@ -2,11 +2,20 @@ const Joi = require('joi');
 
 const generateReport = {
   body: Joi.object().keys({
+    title: Joi.string().allow('', null),
+    summary: Joi.string().allow('', null),
     introduction: Joi.string().allow('', null),
     conclusion: Joi.string().allow('', null),
   }),
 };
 
+const generatePdf = {
+  body: Joi.object().keys({
+    version: Joi.number().integer().min(1),
+  }),
+};
+
 module.exports = {
   generateReport,
+  generatePdf,
 };
