16e4d0f976844b9a69debeac0b861461fca162ba
kundanray1 <raykundan57@gmail.com>
2025-12-27 14:53:40 +0545

    added frontend url for email verification

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -31,8 +31,8 @@ const sendEmail = async (to, subject, text) => {
  */
 const sendResetPasswordEmail = async (to, token) => {
   const subject = 'Reset password';
-  // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `http://link-to-app/reset-password?token=${token}`;
+  const frontendBase = config.frontendUrl && config.frontendUrl !== '*' ? config.frontendUrl : 'http://localhost:5173';
+  const resetPasswordUrl = `${frontendBase.replace(/\/$/, '')}/reset-password?token=${token}`;
   const text = `Dear user,
 To reset your password, click on this link: ${resetPasswordUrl}
 If you did not request any password resets, then ignore this email.`;
@@ -47,8 +47,8 @@ If you did not request any password resets, then ignore this email.`;
  */
 const sendVerificationEmail = async (to, token) => {
   const subject = 'Email Verification';
-  // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `http://link-to-app/verify-email?token=${token}`;
+  const frontendBase = config.frontendUrl && config.frontendUrl !== '*' ? config.frontendUrl : 'http://localhost:5173';
+  const verificationEmailUrl = `${frontendBase.replace(/\/$/, '')}/verify-email?token=${token}`;
   const text = `Dear user,
 To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`;
